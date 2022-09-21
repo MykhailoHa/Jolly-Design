cbefae90601bb1492b5ed15262a98b39d8bf9e12
MykhailoHa <mykhailo.harbuz@gmail.com>
2022-09-22 02:19:42 +0300

    subheader-slider update

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 
 $(document).ready(function(){
-    $('.subheader-slider').slick()
+    $('.subheader-slider').slick({
+        prevArrow: "<img src='img/svg/icons/icon-left.svg' class='sudheader_prev' >",
+        nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >",
+    })
 });
