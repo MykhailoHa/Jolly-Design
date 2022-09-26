837add9f6bf1a73d66b3bb1b98e8362348696821
MykhailoHa <mykhailo.harbuz@gmail.com>
2022-09-26 21:33:51 +0300

    block discover. work with slider

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-  $('.subheader-slider').slick({
+  $('.subheader-slider ').slick({
     prevArrow: "<img src='img/svg/icons/icon-left.svg' class='sudheader_prev' >",
     nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >"
+  });
+  $('.discower-block--slider').slick({
+    prevArrow: "<img src='img/svg/icons/Chevron-left-large.png' class='discower_prev' >",
+    nextArrow: "<img src='img/svg/icons/Chevron-right-large.png' class='discower_next' >"
   });
 });
 //# sourceMappingURL=script.js.map
