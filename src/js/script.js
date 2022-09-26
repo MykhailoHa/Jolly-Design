e299d8f52911a4bfb41eb9e9de411a51b58ae131
MykhailoHa <mykhailo.harbuz@gmail.com>
2022-09-26 23:38:42 +0300

    small update discover block + header fixes

--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         prevArrow: "<img src='img/svg/icons/icon-left.svg' class='sudheader_prev' >",
         nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >",
     })
-    $('.discower-block--slider').slick({
-        prevArrow: "<img src='img/svg/icons/Chevron-left-large.png' class='discower_prev' >",
-        nextArrow: "<img src='img/svg/icons/Chevron-right-large.png' class='discower_next' >",
+    $('.discover-block--slider').slick({
+        prevArrow: "<img src='img/svg/icons/Chevron-left-large.png' class='discover_prev' >",
+        nextArrow: "<img src='img/svg/icons/Chevron-right-large.png' class='discover_next' >",
     })
 });
