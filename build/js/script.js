e4984c66d616604afa20daae299adada4e00bdd2
MykhailoHa <mykhailo.harbuz@gmail.com>
2022-12-08 19:18:33 +0200

    few fixes subheader (before adaptive)

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
   $('.subheader-slider ').slick({
     prevArrow: "<img src='img/svg/icons/icon-left.svg' class='sudheader_prev' >",
-    nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >"
+    nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >",
+    slidesToShow: 3,
+    slidesToScroll: 3
   });
   $('.discover-block--slider').slick({
     prevArrow: "<img src='img/svg/icons/Chevron-left-large.png' class='discover_prev' >",
--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     $('.subheader-slider ').slick({
         prevArrow: "<img src='img/svg/icons/icon-left.svg' class='sudheader_prev' >",
         nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >",
+        slidesToShow :3,
+        slidesToScroll:3
     })
     $('.discover-block--slider').slick({
         prevArrow: "<img src='img/svg/icons/Chevron-left-large.png' class='discover_prev' >",
