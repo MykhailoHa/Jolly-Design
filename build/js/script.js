ff1b8ea1332b26abadd4226fc0c9f57bace1d0a2
MykhailoHa <mykhailo.harbuz@gmail.com>
2022-12-08 19:43:19 +0200

    sub header - adaptive done

--- build/js/script.js
+++ build/js/script.js
@@ -3,7 +3,20 @@ $(document).ready(function () {
     prevArrow: "<img src='img/svg/icons/icon-left.svg' class='sudheader_prev' >",
     nextArrow: "<img src='img/svg/icons/icon-right.svg' class='sudheader_next' >",
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [{
+      breakpoint: 1050,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
   });
   $('.discover-block--slider').slick({
     prevArrow: "<img src='img/svg/icons/Chevron-left-large.png' class='discover_prev' >",
